69445cb838befe417cef742d093eceb2afe0b386
ARIANE PAULA BARROS <ariane.barros@ifood.com.br>
2023-05-17 11:21:29 -0300

    fix comparison

--- comparison.py
+++ comparison.py
@@ -1,4 +1,3 @@
-
 import numpy as np 
 import pandas as pd 
 import os
@@ -9,42 +8,39 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import IsolationForest
 import matplotlib.pyplot as plt
+import datetime
 
-dataset_name = "./User0_credit_card_transactions.csv"
+dataset_name = "./filtered.csv"
 
 df = pd.read_csv(dataset_name)
 df.info()
-print("finished loading")
 
-for col in df.columns:
-    col_type = df[col].dtype
-    if col_type == 'object' or col_type.name == 'category' or col == 'Time':
-        df[col] = df[col].astype('category')
+my_date = datetime.datetime(2023,5,5)
+converted_timestamps = {}
+for i in df['Time']:
+    timestamp = datetime.datetime.timestamp(my_date)
+    time = str(i).split(':')
+    my_time = datetime.time(int(time[0]), int(time[1]))
+    combined_datetime = my_date.combine(my_date, my_time)
+    timestamp = datetime.datetime.timestamp(combined_datetime)
+    converted_timestamps[i] = timestamp
 
-y = df['Is Fraud?']
+df['Time'] = df['Time'].replace(converted_timestamps)
 
-X = df.drop(['Is Fraud?'],axis=1)
 
-X_train_lgbm, X_test_lgbm, y_train_lgbm, y_test_lgbm = train_test_split(X, y, test_size = 0.3, random_state = 0, stratify=y)
+amounts = {}
+for i in df['Amount']:
+    amounts[i] = float(str(i).split('$')[1])
+df['Amount'] = df['Amount'].replace(amounts)
 
-print('LGBM....')
-model_lgbm = lgb.LGBMClassifier()
-model_lgbm.fit(X_train_lgbm, y_train_lgbm, feature_name='auto', categorical_feature = 'auto', verbose=50)
-
-y_pred_lgbm=model_lgbm.predict(X_test_lgbm)
-print("LGBM done!")
-
-df = pd.read_csv(dataset_name)
-
-df.drop(['User'],axis=1)
-df.drop(['Card'],axis=1)
-df.drop(['Merchant State'],axis=1)
-df.drop(['Zip'],axis=1)
-df.drop(['MCC'],axis=1)
-df.drop(['Errors?'],axis=1)
-y = df['Is Fraud?']
-
-X = df.drop(['Is Fraud?'],axis=1)
+for c in ['Use Chip','Merchant City', 'Merchant State', 'Errors?', 'Is Fraud?']:
+    values = {}
+    count = 0
+    for i in df[c]:
+        if values.get(i) is None:
+            values[i] = count
+            count += 1
+    df[c] = df[c].replace(values)
 
 #Normalização
 df['User'] = df['User'].fillna(df['User'].mode()[0])
@@ -54,8 +50,8 @@ df['Month'] = df['Month'].fillna(df['Month'].mode()[0])
 df['Day'] = df['Day'].fillna(df['Day'].mode()[0])
 df['Time'] = df['Time'].fillna(df['Time'].mode()[0])
 #df['Amount'] = pd.to_numeric(df['Amount'], errors='coerce')
-#df['Amount'] = df['Amount'].fillna(df['Amount'].mean())#.fillna(df['Amount'].shift() + df['Amount'].shift(-1)/2)
-df.dropna(subset = ['Amount'])
+df['Amount'] = df['Amount'].fillna(df['Amount'].mean())#.fillna(df['Amount'].shift() + df['Amount'].shift(-1)/2)
+#df.dropna(subset = ['Amount'])
 df['Use Chip'] = df['Use Chip'].fillna(df['Use Chip'].mode()[0])
 df['Merchant Name'] = df['Merchant Name'].fillna(df['Merchant Name'].mode()[0])
 df['Merchant City'] = df['Merchant City'].fillna(df['Merchant City'].mode()[0])
@@ -71,10 +67,19 @@ for col in df.columns:
         print(col)
 print("Check for Nan done!")
 
+df.info()
+y = df['Is Fraud?']
 
-X = pd.get_dummies(df)
+X = df.drop(['Is Fraud?'],axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 42, stratify=y)
+
+print('LGBM....')
+model_lgbm = lgb.LGBMClassifier()
+model_lgbm.fit(X_train, y_train, feature_name='auto', categorical_feature = 'auto', verbose=50)
+
+y_pred_lgbm=model_lgbm.predict(X_test)
+print("LGBM done!")
 
 print("Logictic regression....")
 model_lr = LogisticRegression(solver='lbfgs', max_iter=400)
@@ -101,7 +106,7 @@ print("Logictic regression done!")
 
 # Avaliação
 print('LGBM Report')
-print(classification_report(y_test_lgbm, y_pred_lgbm))
+print(classification_report(y_test, y_pred_lgbm))
 importances = model_lgbm.feature_importances_
 importances_greater_than_zero = []
 for i in importances:
