948b45f596df232c76ec6e2ca5bfda38119de364
pavanann <luccapavansilva@hotmail.com>
2023-06-01 22:59:53 -0300

    front lgbm

--- front.py
+++ front.py
@@ -1,32 +1,39 @@
 import streamlit as st
 import datetime
+import lgbm
 
-def search_data(year, month, day, hour, value, data_type, name, city):
-    # Your search logic goes here
-    # Implement the search algorithm based on the provided inputs
-    # Return True or False based on the search result
-    # You can replace the logic below with your own implementation
-    if year == 2023 and month == 5 and day == 22 and hour == 10 and value == 42 and \
-            data_type == 'Type A' and name == 'John' and city == 'New York':
-        return True
+
+def search_data(year: int, month: int, day: int, time: float, amount: float, use_chip: int, merchant_name: int, merchant_city: int):
+    predicted = lgbm([[year, month, day, time, amount, use_chip, merchant_name, merchant_city]])
+    if predicted[0] == 0:
+        result = "OK"
     else:
-        return False
+        result = "FRAUDE"
+    return result
 
 def main():
     # Create input fields
     st.title('Data Search')
 
-    time = st.time_input('Hora da Transação', datetime.time(8, 45))
-    year = st.date_input('Data da Transação')
-    value = st.number_input('Valor da Transação', min_value=0.0, value=42.0)
-    data_type = st.selectbox('Tipo da Transação', ['Crédito', 'Débito', 'Boleto', 'TED', 'DOC', 'PIX', 'Cheque'])
-    name = st.text_input('Nome do Transador')
-    city = st.text_input('Cidade da Transação')
+
+    result = st.empty()
+
+    year = st.number_input('Ano da Transação', min_value=0, value=2022, max_value=2023)
+    month = st.number_input('Mes da Transação', min_value=1, value=1, max_value=12)
+    day = st.number_input('Dia da Transação', min_value=0, value=15, max_value=31)
+    time = st.number_input('Hora da Transação', min_value=0, value=12, max_value=24)
+    amount = st.number_input('Valor da Transação', min_value=0.0, value=42.0)
+    use_chip = st.number_input('Tipo da Transação')
+    merchant_name = st.number_input('Nome do Estabelecimento')
+    merchant_city = st.number_input('Cidade da Transação')
+
+    botao = st.button('Search')
 
     # Search button
-    if st.button('Search'):
-        result = search_data(year, time, value, data_type, name, city)
-        st.write(f'Search Result: {result}')
-
+    if botao:
+        result_text = search_data(year, month, day, time, amount, use_chip, merchant_name, merchant_city)
+        col1, col2, col3 = st.columns(3)
+        col1.metric("", result_text, "")
+    
 if __name__ == '__main__':
     main()
